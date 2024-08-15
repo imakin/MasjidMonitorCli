ffa30195c3f19a9bdd5aebcdfac1d60823bec422
izzulmakin <makin.ugm@gmail.com>
2024-08-15 11:18:42 +0700

    add launcher

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+try:
+    import pyfiglet
+except ModuleNotFoundError:
+    os.system('sudo pip install pipx --break-system-packages')
+    os.system('pipx install pyfiglet')
+    os.system(f'python3 {os.path.dirname(os.path.realpath(sys.argv[0]) )}/app.py')
+    exit(0)
+
+import sys
+
+basedir = os.path.dirname(os.path.realpath(sys.argv[0]) )
+os.chdir(f"{basedir}/source/app")
+sys.path.append(f"{basedir}/source/app")
+
+print(os.listdir('.'))
+
+import main
+
+if __name__=="__main__":
+    main.curses.wrapper(main.main)
